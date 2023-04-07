ba5d3bda4414d56bf77dbd3eaca81c386ba31f96
Psycholav <129789058+Psycholav@users.noreply.github.com>
2023-04-08 04:53:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 """Config fill properly"""
 import os
 
-API_ID = int(os.environ.get("API_ID", "6435225"))
-API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+API_ID = int(os.environ.get("API_ID", "20154655"))
+API_HASH = os.environ.get("API_HASH", "e9c63cacf52d59607b769f13140af417")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
 DB_URI = os.environ.get("ELEPHANT_SQL", "")
